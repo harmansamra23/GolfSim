0b484c7248a947a0cad70de741e8c3961dc05c9d
harmansamra23 <harmansamra64@gmail.com>
2026-08-10 17:36:01 -0700

    Phase 2: compose basic 3D hole renderer

---
+++ src/course/CourseHole.tsx
@@ -0,0 +1,21 @@
+import { Bunkers } from './Bunkers'
+import { Fairway } from './Fairway'
+import { Flag } from './Flag'
+import { Green } from './Green'
+import { TeeBox } from './TeeBox'
+import { Terrain } from './Terrain'
+import { Vegetation } from './Vegetation'
+
+export function CourseHole() {
+  return (
+    <>
+      <Terrain />
+      <Fairway />
+      <TeeBox />
+      <Green />
+      <Bunkers />
+      <Flag />
+      <Vegetation />
+    </>
+  )
+}
