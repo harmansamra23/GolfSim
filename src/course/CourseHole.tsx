2d60de50676dc5874f48d747808c0c5e5df45a62
harmansamra23 <harmansamra64@gmail.com>
2026-08-11 04:03:37 -0700

    Phase 14: compose course from active hole data

--- src/course/CourseHole.tsx
+++ src/course/CourseHole.tsx
@@ -1,3 +1,4 @@
+import type { GolfHole } from '../courses/courseTypes'
 import { Bunkers } from './Bunkers'
 import { Fairway } from './Fairway'
 import { Flag } from './Flag'
@@ -7,17 +8,17 @@ import { TeeBox } from './TeeBox'
 import { Terrain } from './Terrain'
 import { Vegetation } from './Vegetation'
 
-export function CourseHole() {
+export function CourseHole({ hole }: { hole: GolfHole }) {
   return (
     <>
-      <Terrain />
-      <Fairway />
-      <TeeBox />
-      <Green />
-      <Bunkers />
-      <Flag />
-      <Vegetation />
-      <GrassDetail />
+      <Terrain hole={hole} />
+      <Fairway hole={hole} />
+      <TeeBox hole={hole} />
+      <Green hole={hole} />
+      <Bunkers hole={hole} />
+      <Flag hole={hole} />
+      <Vegetation hole={hole} />
+      <GrassDetail hole={hole} />
     </>
   )
 }
