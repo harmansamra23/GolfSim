71972c0f594134bccae758cc5215a6520b088da2
harmansamra23 <harmansamra64@gmail.com>
2026-08-18 12:34:50 -0700

    Seat cup and flag on raised green

--- src/course/Flag.tsx
+++ src/course/Flag.tsx
@@ -1,15 +1,21 @@
 import * as THREE from 'three'
 
 import type { GolfHole } from '../courses/courseTypes'
+import { terrainHeightAtPosition } from './terrainHeight'
 
 export function Flag({ hole }: { hole: GolfHole }) {
   const green = hole.green
+  const greenY = terrainHeightAtPosition(
+    hole,
+    green.center.x,
+    green.center.z
+  )
 
   return (
     <>
       <mesh
         rotation={[-Math.PI / 2, 0, 0]}
-        position={[green.center.x, 0.082, green.center.z]}
+        position={[green.center.x, greenY + 0.076, green.center.z]}
       >
         <circleGeometry args={[0.11, 32]} />
         <meshStandardMaterial color="#0b0b0b" roughness={1} />
@@ -17,14 +23,14 @@ export function Flag({ hole }: { hole: GolfHole }) {
 
       <mesh
         rotation={[-Math.PI / 2, 0, 0]}
-        position={[green.center.x, 0.084, green.center.z]}
+        position={[green.center.x, greenY + 0.079, green.center.z]}
       >
         <ringGeometry args={[0.108, 0.135, 32]} />
         <meshStandardMaterial color="#f1f1e8" roughness={0.7} />
       </mesh>
 
       <mesh
-        position={[green.center.x, 2.25, green.center.z]}
+        position={[green.center.x, greenY + 2.25, green.center.z]}
         castShadow
       >
         <cylinderGeometry args={[0.025, 0.025, 4.3, 12]} />
@@ -32,7 +38,7 @@ export function Flag({ hole }: { hole: GolfHole }) {
       </mesh>
 
       <mesh
-        position={[green.center.x + 0.7, 3.7, green.center.z]}
+        position={[green.center.x + 0.7, greenY + 3.7, green.center.z]}
         castShadow
       >
         <planeGeometry args={[1.4, 0.72]} />
